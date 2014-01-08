75fbe4eafffddf4c1e06dd56c8cb25916804b12b
Brian Lalor <brian@autosportlabs.com>
2014-01-08 06:42:57 -0500

    Add Logstash template
    Temporary until we switch to LogStash 1.3, which includes this
    template.  We still may want to use our own, but that can be done from
    LogStash.

--- src/config.sh
+++ src/config.sh
@@ -5,9 +5,12 @@ set -x
 
 cd /tmp/src
 
-mv elasticsearch-logrotate.conf /etc/logrotate.d/elasticsearch
+mv templates                 /etc/elasticsearch/templates
 mv elasticsearch-logging.yml /etc/elasticsearch/logging.yml
-mv elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+mv elasticsearch.yml         /etc/elasticsearch/elasticsearch.yml
+
+mv elasticsearch-logrotate.conf /etc/logrotate.d/elasticsearch
+
 mv supervisord.conf /etc/supervisor.d/elasticsearch.conf
 
 mv launch.sh /usr/local/bin/launch-elasticsearch.sh
